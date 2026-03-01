7366dc273d423f0cbd25f1de6a63eb38bfcd14e7
jayBeeCool <jB@mira1.net>
2026-03-01 22:09:33 +0100

    test: drop stray root gregorian vectors file (kept canonical tests)

--- test/current_from_legacy/F01_GregorianVectors.t.sol
+++ test/current_from_legacy/F01_GregorianVectors.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.24;
 
 import "forge-std/Test.sol";
-import {Gregorian} from "contracts/lib/Gregorian.sol";
+import "contracts/lib/Gregorian.sol";
 
 contract F01_GregorianVectors_Test is Test {
     function test_yearOf_epoch() public {
