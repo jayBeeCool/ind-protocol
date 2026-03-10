d275d508f478b421f5c994fcce9b89230a97bd0b
jayBeeCool <jB@mira1.net>
2026-03-10 17:00:58 +0100

    deploy sepolia sale stack on existing IND with open receiver

---
+++ contracts/INDOpenDepositReceiver.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+interface IINDSale {
+    function purchaseFor(address recipient, address refundTo) external payable;
+}
+
+contract INDOpenDepositReceiver {
+    IINDSale public immutable sale;
+
+    error ZeroValue();
+
+    constructor(address sale_) {
+        require(sale_ != address(0), "sale=0");
+        sale = IINDSale(sale_);
+    }
+
+    receive() external payable {
+        _forward(msg.sender);
+    }
+
+    function deposit() external payable {
+        _forward(msg.sender);
+    }
+
+    function _forward(address recipient) internal {
+        if (msg.value == 0) revert ZeroValue();
+        sale.purchaseFor{value: msg.value}(recipient, recipient);
+    }
+}
---
+++ script/DeploySepoliaSaleStack.s.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "forge-std/Script.sol";
+import "../contracts/InheritanceDollar.sol";
+import "../contracts/INDSale.sol";
+import "../contracts/INDOpenDepositReceiver.sol";
+
+contract DeploySepoliaSaleStack is Script {
+    // vecchio IND Sepolia con 10B già mintati
+    address constant IND_ADDR  = 0xc9a8f1017665d64a7e809886b7df51dc232d3051;
+
+    // vero admin/minter del vecchio stack
+    address constant SAFE_ADMIN = 0x0527A7671b4B05f20678b8D93EF072c15F6E4aF9;
+
+    function run() external {
+        vm.startBroadcast();
+
+        InheritanceDollar ind = InheritanceDollar(IND_ADDR);
+        INDSale sale = new INDSale(SAFE_ADMIN, address(ind));
+        INDOpenDepositReceiver receiver = new INDOpenDepositReceiver(address(sale));
+
+        vm.stopBroadcast();
+
+        console2.log("IND      :", address(ind));
+        console2.log("SAFE     :", SAFE_ADMIN);
+        console2.log("SALE     :", address(sale));
+        console2.log("RECEIVER :", address(receiver));
+    }
+}
